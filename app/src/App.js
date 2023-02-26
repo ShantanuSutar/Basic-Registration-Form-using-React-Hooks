c988e0ce2404733ae09f8b48ca166892db6e05c9
Shantanu Sutar <shantanusutar17@gmail.com>
2023-02-26 21:21:43 +0530

    taking inputs

--- app/src/App.js
+++ app/src/App.js
@@ -1,39 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  const firstNameChange = (e) => {
+    setValues({ ...values, firstName: e.target.value });
+  };
+  const lastNameChange = (e) => {
+    setValues({ ...values, lastName: e.target.value });
+  };
+  const email = (e) => {
+    setValues({ ...values, email: e.target.value });
+  };
   return (
     <div class="form-container">
       <form class="register-form">
-        {/* Uncomment the next line to show the success message */}
-        {/* <div class="success-message">Success! Thank you for registering</div> */}
+        <div class="success-message">Success! Thank you for registering</div>
         <input
           id="first-name"
           class="form-field"
           type="text"
           placeholder="First Name"
           name="firstName"
+          onChange={firstNameChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="first-name-error">Please enter a first name</span> */}
+
+        <span id="first-name-error">Please enter a first name</span>
         <input
           id="last-name"
           class="form-field"
           type="text"
           placeholder="Last Name"
           name="lastName"
+          onChange={lastNameChange}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="last-name-error">Please enter a last name</span> */}
+
+        <span id="last-name-error">Please enter a last name</span>
         <input
           id="email"
           class="form-field"
           type="text"
           placeholder="Email"
           name="email"
+          onChange={email}
         />
-        {/* Uncomment the next line to show the error message */}
-        {/* <span id="email-error">Please enter an email address</span> */}
+
+        <span id="email-error">Please enter an email address</span>
         <button class="form-field" type="submit">
           Register
         </button>
